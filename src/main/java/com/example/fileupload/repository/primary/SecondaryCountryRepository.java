e825492434e9deb09de42c7170cdb94e69bb94ea
anubhavanu <jyotsna.shukla@timesinternet.in>
2023-09-22 20:33:27 +0530

    local properties file added d

--- src/main/java/com/example/fileupload/repository/primary/SecondaryCountryRepository.java
+++ src/main/java/com/example/fileupload/repository/primary/SecondaryCountryRepository.java
@@ -1,6 +1,6 @@
-package com.example.fileupload.repository.secondary;
+package com.example.fileupload.repository.primary;
 
-import com.example.fileupload.model.secondary.SecondaryCountryImp;
+import com.example.fileupload.model.primary.SecondaryCountryImp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SecondaryCountryRepository extends JpaRepository<SecondaryCountryImp, String> {
