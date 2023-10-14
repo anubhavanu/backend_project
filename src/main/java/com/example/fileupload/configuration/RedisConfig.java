a908b0352c63d1bb360321f3a23addf963250508
anubhavanu <jyotsna.shukla@timesinternet.in>
2023-10-14 13:21:41 +0530

    dockerfile added

--- src/main/java/com/example/fileupload/configuration/RedisConfig.java
+++ src/main/java/com/example/fileupload/configuration/RedisConfig.java
@@ -36,7 +36,7 @@ public class RedisConfig  {
 
     @Bean
     public LettuceBasedProxyManager set_proxy_mgr() {
-        io.lettuce.core.RedisClient redisClient = RedisClient.create("redis://user:password/ec2-13-233-123-95.ap-south-1.compute.amazonaws.com/6379" );
+        io.lettuce.core.RedisClient redisClient = RedisClient.create("redis://user:password/ec2-13-233-123-95.ap-south-1.compute.amazonaws.com/6379");
         LettuceBasedProxyManager proxyManager = LettuceBasedProxyManager.builderFor(redisClient).build();
         return proxyManager;
     }
