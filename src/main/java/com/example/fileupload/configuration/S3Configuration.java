56f2ed5bfadcd5ed9b176b233ebdc06483d5ea15
anubhavanu <jyotsna.shukla@timesinternet.in>
2023-10-16 20:00:22 +0530

    dockerfile added

---
+++ src/main/java/com/example/fileupload/configuration/S3Configuration.java
@@ -0,0 +1,25 @@
+package com.example.fileupload.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Configuration {
+    @Value("${AWS_ACCESS_KEY_ID}")
+    private String accessKey;
+    @Value("${AWS_SECRET_ACCESS_KEY}")
+    private String secretKey;
+    @Bean
+    public S3Client  s3Client(){
+        AwsCredentialsProvider credentialsProvider =  EnvironmentVariableCredentialsProvider.create();
+        S3Client s3Client=S3Client.builder()
+                .credentialsProvider(credentialsProvider)
+                .region(Region.AP_SOUTH_1).build();
+        return s3Client;
+    }
+}
